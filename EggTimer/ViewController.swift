bf53db0f608d3a48f5da6c11bb4d99e3aa58fb0b
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-04 21:00:24 +0530

    Added progress bar to indicate the time to wait on UI

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -14,9 +14,12 @@ class ViewController: UIViewController {
     let eggTimes = ["Soft":3, "Medium": 4, "Hard": 7]
     
     @IBOutlet weak var startText: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    var progressValue: Float = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        progressBar.progress = progressValue
         
     }
 
@@ -25,10 +28,12 @@ class ViewController: UIViewController {
         
         //everytime button is pressed, we stop the previous timers and start it afresh
         timer.invalidate()
+        progressBar.progress = progressValue
+        startText.text = "How do you like your eggs?"
         let hardness = sender.currentTitle
         
         counter = eggTimes[hardness!]!
-        
+        progressValue = Float(1)/Float(counter)
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
         print("\(counter)")
         
@@ -38,6 +43,7 @@ class ViewController: UIViewController {
     @objc func updateCounter() {
         if counter > 0 {
             print("\(counter) seconds")
+            progressBar.progress += progressValue
             counter -= 1
         }
         else {
